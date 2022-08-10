a0d873dda0b24ace3eac5f52c4b82a3b5d96ccc8
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-08-11 01:28:00 +0800

    Auto enable

--- src/Plugin/MainClass.php
+++ src/Plugin/MainClass.php
@@ -9,7 +9,11 @@ use pocketmine\plugin\PluginBase;
 
 final class MainClass extends PluginBase {
 	public function onEnable() : void{
-		CombatMode::$std = AwaitStd::init($this);
+		CombatMode::$std = $std = AwaitStd::init($this);
+		CombatMode::autoEnable(function () use ($std) : \Generator {
+			// Each combat mode will least for 15 seconds. The timer resets on damage.
+			yield from $std->sleep(15 * 20);
+		});
 	}
 
 	public function onDisable() : void{
